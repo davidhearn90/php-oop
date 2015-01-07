56d560c11c56c980d038c822add648715ad1b79b
David Hearn <davidhearn90@gmail.com>
2015-01-06 22:14:32 -0800

    classes

--- lecture.php
+++ lecture.php
@@ -1,1 +1,58 @@
-test1
+<?php
+
+class You{
+
+	public $firstname = "bob";
+	public $lastname = "jackson";
+	public $age = "15";
+
+	function name(){
+		return"{$this->firstname}" .
+		"{$this->lastname}";
+
+	}
+
+	$me = new You();
+	$me->firstname = "David";
+	$me->lastname = "Hearn";
+
+	print "my name is {$me->name()}";
+
+class Duck{
+
+	public $firstname = "bob";
+	public $lastname = "jackson";
+	public $age = "15";
+
+	function name(){
+		return"{$this->firstname}" .
+		"{$this->lastname}";
+
+	}
+
+	$farm = new Duck();
+	$farm->firstname = "Donald";
+	$farm->lastname = "Duck";
+
+	print "my name is {$farm->name()}";
+
+class chicken{
+
+	public $firstname = "bob";
+	public $lastname = "jackson";
+	public $age = "15";
+
+	function name(){
+		return"{$this->firstname}" .
+		"{$this->age}";
+
+	}
+
+	$farm = new chicken();
+	$farm->firstname = "chick";
+	$farm->age = "3";
+
+	print "my name is {$farm->name()}";
+
+	
+	
